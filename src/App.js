e591fe2b42443f1e6097599a34617d4b9c5f0e0f
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-06-12 12:31:37 -0400

    passed number of events state to numberOfEvents component as prop. Created function to update state as number input changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import { getEvents, extractLocations } from './api';
 class App extends Component{
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 32,
+    selectedLocation: "all"
   }
 
   componentDidMount(){
+    const { numberOfEvents } = this.state;
     getEvents().then((events) => {
-      this.setState({ events, locations: extractLocations(events) });
+      this.setState({ events: events.slice(0, numberOfEvents), locations: extractLocations(events) });
     });
   }
 
@@ -23,20 +26,40 @@ class App extends Component{
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ? events : events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
+  updateEvents = (location, eventCount) => {
+    const { selectedLocation, numberOfEvents } = this.state;
+    if(location){
+      getEvents().then((events) => {
+        const locationEvents = 
+        (location === 'all')
+        ? getEvents
+        : events.filter(
+          (event) => event.location === location);
+        const filterEvents = locationEvents.slice(0, numberOfEvents);
+        this.setState({
+          events: filterEvents,
+          selectedLocation: location 
       });
     });
-  }
+  } else {
+    getEvents().then(events => {
+      const locationEvents = (selectedLocation === 'all')
+      ? events
+      : events.filter(event => event.location === selectedLocation);
+      const filterEvents = locationEvents.slice(0, eventCount);
+      this.setState({
+        events: filterEvents,
+        numberOfEvents: eventCount
+      });
+    });
+  }  
+}
 
   render(){
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <NumberOfEvents />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents = {this.updateEvents}/>
         <EventList events={this.state.events}/>
       </div>
     );
