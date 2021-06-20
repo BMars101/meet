a5cab7e7045d7f04da32f69ebeb975f2acf37c0c
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-06-20 13:59:35 -0400

    imported error alert to check for correct number of eventsto show.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
@@ -8,10 +9,18 @@ class NumberOfEvents extends Component {
 
   handleChange = (event) => {
     const value = event.target.value;
-    this.props.updateEvents(null, value);
-    this.setState({
-      numOfEvents: value
-    });
+    if(value < 1 || value > 32){
+      this.setState({
+        errorText: 'Please enter a number between 1 and 32',
+        numOfEvents: ''
+      })
+    }else {
+      this.setState({
+        numOfEvents: value,
+        errorText: ''
+      });
+      this.props.updateEvents(null, value);
+    }
   }
 
 
@@ -26,6 +35,7 @@ class NumberOfEvents extends Component {
           placeholder='Enter number of events to view'
           onChange={this.handleChange}
         />
+        <ErrorAlert text={this.state.errorText}/>
       </div>
     )
   }
